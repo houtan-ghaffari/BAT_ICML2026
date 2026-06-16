3968f704998538ffd57f974ea9fff284b2adad93
Houtan Ghaffari <houtan.ghaffari@gmail.com>
2026-06-16 13:55:59 +0200

    Refactor pretrain.py

--- pretrain.py
+++ pretrain.py
@@ -6,16 +6,17 @@ __author__ = "Houtan Ghaffari"
 __email__ = "houtan.ghaffari@gmail.com"
 __version__ = "1.0.0"
 
-import torch
 from yaml_config import get_ssl_config
-from engine.ssl_trainer import run_ssl_experiment
 from engine.utils import seed_everything
+from engine.ssl_trainer import run_ssl_experiment
+
 
 def main() -> None:
     seed_everything()
     args = get_ssl_config()
-    print(f"CUDA Available: {torch.cuda.is_available()}")
     run_ssl_experiment(args)
+
 
 if __name__ == '__main__':
     main()
+
